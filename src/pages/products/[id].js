731e98789aed8cc5385dfbe532ae78d89583fe54
Blaine Kasten <blainekasten@gmail.com>
2020-06-03 15:11:58 -0500

    updated examples

--- src/pages/products/[id].js
+++ src/pages/products/[id].js
@@ -1,22 +1,5 @@
 import React from "react"
-import { graphql, createPagesFromData } from "gatsby"
 
-function Product(props) {
-  return (
-    <div>
-      <h1>{props.data.product.name}</h1>
-      <h3>{props.data.product.id}</h3>
-    </div>
-  )
+export default function NotFoundProductRoute() {
+  return <h1>Data not found</h1>
 }
-
-export default createPagesFromData(Product, `Product`)
-
-export const query = graphql`
-  query Product($id: String) {
-    product(id: { eq: $id }) {
-      id
-      name
-    }
-  }
-`
