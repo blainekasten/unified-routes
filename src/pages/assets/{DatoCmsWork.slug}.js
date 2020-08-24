05f0bd3681c7ad7e1820ea1d77ec526e3080e08f
Blaine Kasten <blainekasten@gmail.com>
2020-08-24 10:42:50 -0500

    new version

--- src/pages/assets/{DatoCmsWork.slug}.js
+++ src/pages/assets/{DatoCmsWork.slug}.js
@@ -1,11 +1,14 @@
 import React from "react"
 import { graphql } from "gatsby"
+import Img from "gatsby-image"
 
 export default function Page(props) {
+  console.log(props)
   return (
     <>
       <h1>Excerpt: </h1>
       <p>{props.data.datoCmsWork.excerpt}</p>
+      <Img fluid={props.data.datoCmsWork.coverImage.fluid} />
     </>
   )
 }
@@ -14,6 +17,11 @@ export const query = graphql`
   query DatoWork($id: String) {
     datoCmsWork(id: { eq: $id }) {
       excerpt
+      coverImage {
+        fluid {
+          ...GatsbyDatoCmsFluid
+        }
+      }
     }
   }
 `
